f35f8f30423741fee60dc372237e3a1f40b0b377
Mary <SoftDev@SOFTDEV>
2019-06-06 14:58:39 -0600

    Add project files.

---
+++ mvctest/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvctest.Models
+{
+    public partial class Products
+    {
+        public int ProductId { get; set; }
+        public string ProdName { get; set; }
+    }
+}
