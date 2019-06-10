57c729f085f7772d340189d2fa8d18c17973050a
Jon Arca <jonathan.arca@edu.sait.ca>
2019-06-10 13:58:26 -0600

    -registration validation

--- mvctest/Models/Customers.cs
+++ mvctest/Models/Customers.cs
@@ -8,18 +8,26 @@ namespace mvctest.Models
     public partial class Customers
     {
         public int CustomerId { get; set; }
-        [Required(ErrorMessage = "Please enter your name")]
+        [Required(ErrorMessage = "Please enter your first name")]
         public string CustFirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
         public string CustLastName { get; set; }
+        [Required(ErrorMessage = "Please enter your address")]
         public string CustAddress { get; set; }
+        [Required(ErrorMessage = "Please enter your city")]
         public string CustCity { get; set; }
+        [Required(ErrorMessage = "Please enter your province")]
         public string CustProv { get; set; }
+        [Required(ErrorMessage = "Please enter your postal code")]
         public string CustPostal { get; set; }
         public string CustCountry { get; set; }
         public string CustHomePhone { get; set; }
+        [Required(ErrorMessage = "Please enter your business phone number")]
         public string CustBusPhone { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
         public string CustEmail { get; set; }
         public int? AgentId { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
         public string PasswordNotHashed { get; set; }
         public byte[] PasswordHashed { get; set; }
         public Guid? PasswordSalt { get; set; }
